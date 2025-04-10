d834849bcbf9bd4f0c68fb740b8c20c70b853327
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-10 09:13:56 -0400

    ajusta contenido

--- myapp/tests.py
+++ myapp/tests.py
@@ -6,7 +6,7 @@ class PingTests(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_endpoint_ping_responde_con_pong(self):
+    def test_endpoint_ping_responde_ping_pong(self):
         response = self.client.get('/ping/')
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {'ping': 'pong'})
