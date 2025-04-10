c505cff0cbf24111a03b7ad330cfd0e25c0e98c6
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-10 01:03:18 -0400

    complementa pruebas

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,6 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from myapp.views import sumar
 
+# Test de la vista ping
+class PingTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_ping(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'ping': 'pong'})
+
+# Test de la función sumar
 class SumaTests(TestCase):
     def test_sumar_correctamente(self):
         self.assertEqual(sumar(2, 2), 4)
