14a203e6937c352f7cf86e37cb2ad7b9f971e705
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-10 09:16:06 -0400

    corrige salida endpoint

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 def ping(request):
-    return JsonResponse({'ping': 'ttong'})
+    return JsonResponse({'ping': 'pong'})
 
 def sumar(a, b):
     return a + b
