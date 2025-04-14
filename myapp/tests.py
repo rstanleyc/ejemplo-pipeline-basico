61809a9bd179c6b28d559ba8efdada3fa2d9536d
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-14 17:12:29 -0400

    agrega cambios

--- myapp/tests.py
+++ myapp/tests.py
@@ -7,7 +7,7 @@ class PingTests(TestCase):
         self.client = Client()
 
     def test_endpoint_ping_responde_ping_pong(self):
-        response = self.client.get('/ping/')
+        response = self.client.get('/ping /')
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {'ping': 'pong'})
 
