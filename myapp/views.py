4f623bcde5035af58ab99a7eedc767b045a1cdfa
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-14 20:14:21 -0400

    fallo

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 
 def ping(request):
     #soy yo haciendo un cambio
-    return JsonResponse({'ping': 'tong'})
+    return JsonResponse({'ping': 'pong'})
 
 def sumar(a, b):
     return a + b
