f8b20cbf9438338e3ac89a71f17d30cda04ae527
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-14 20:26:22 -0400

    rompe algo

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 
 def ping(request):
     #soy yo haciendo un cambio
-    return JsonResponse({'ping': 'pong'})
+    return JsonResponse({'ping': 'tong'})
 
 def sumar(a, b):
     return a + b
